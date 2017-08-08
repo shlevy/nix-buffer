8f46af044f4736c94292a45e91a78fd1ba837d33
Matthew Bauer <mjbauer95@gmail.com>
2017-08-08 13:36:27 -0700

    use withPackages from nixpkgs

--- nix-buffer.nix
+++ nix-buffer.nix
@@ -4,51 +4,7 @@ let
   pkgs = import <nixpkgs> {};
   inherit (pkgs) lib writeText runCommand;
   inherit (pkgs.emacsPackagesNg) inherit-local;
-  withPackages = pkgs: let
-      extras = map (x: x.emacsBufferSetup pkgs)
-                   (builtins.filter (builtins.hasAttr "emacsBufferSetup") pkgs);
-    in writeText "dir-locals.el" ''
-      (require 'inherit-local "${inherit-local}/share/emacs/site-lisp/elpa/inherit-local-${inherit-local.version}/inherit-local.elc")
-      ; Only set up nixpkgs buffer handling when we have some buffers active
-      (defvar nixpkgs--buffer-count 0)
-      (when (eq nixpkgs--buffer-count 0)
-        (make-variable-buffer-local 'nixpkgs--is-nixpkgs-buffer)
-        (defun nixpkgs--around-generate (orig name)
-          (if (and nixpkgs--is-nixpkgs-buffer (eq (aref name 0) ?\s))
-              (let ((buf (funcall orig name)))
-                (progn
-                  (inherit-local-inherit-child buf)
-                  (with-current-buffer buf
-                    (setq nixpkgs--buffer-count (1+ nixpkgs--buffer-count))
-                    (add-hook 'kill-buffer-hook
-                              'nixpkgs--decrement-buffer-count nil t)))
-                buf)
-            (funcall orig name)))
-        (advice-add 'generate-new-buffer :around #'nixpkgs--around-generate)
-        ; When we have no more nixpkgs buffers, tear down the buffer handling
-        (defun nixpkgs--decrement-buffer-count ()
-          (setq nixpkgs--buffer-count (1- nixpkgs--buffer-count))
-          (when (eq nixpkgs--buffer-count 0)
-            (advice-remove 'generate-new-buffer #'nixpkgs--around-generate)
-            (fmakunbound 'nixpkgs--around-generate)
-            (fmakunbound 'nixpkgs--decrement-buffer-count))))
-      (setq nixpkgs--buffer-count (1+ nixpkgs--buffer-count))
-      (add-hook 'kill-buffer-hook 'nixpkgs--decrement-buffer-count nil t)
-      ; Add packages to PATH and exec-path
-      (make-local-variable 'process-environment)
-      (put 'process-environment 'permanent-local t)
-      (inherit-local 'process-environment)
-      ; setenv modifies in place, so copy the environment first
-      (setq process-environment (copy-tree process-environment))
-      (setenv "PATH" (concat "${lib.makeSearchPath "bin" pkgs}:"
-                     (getenv "PATH")))
-      (inherit-local-permanent exec-path (append
-                               '(${builtins.concatStringsSep " "
-                                 (map (p: "\"${p}/bin\"") pkgs)}) exec-path))
-      (setq nixpkgs--is-nixpkgs-buffer t)
-      (inherit-local 'nixpkgs--is-nixpkgs-buffer)
-      ${lib.concatStringsSep "\n" extras}
-    '';
+  inherit (pkgs.nixBufferBuilders) withPackages;
 in if builtins.pathExists defnix then
   withPackages (import defnix).buildInputs
 else {}
