68340fc73efd6b5ec76ca80f4225b7e4f6920d09
Matthew Bauer <mjbauer95@gmail.com>
2017-08-08 20:43:32 -0700

    simplify nix-buffer.nix

--- nix-buffer.nix
+++ nix-buffer.nix
@@ -2,14 +2,15 @@
 let
   defnix = root + "/default.nix";
   pkgs = import <nixpkgs> {};
-  inherit (pkgs) lib writeText runCommand;
+  inherit (pkgs) writeText runCommand;
+  inherit (pkgs.lib) overrideDerivation isDerivation;
   inherit (pkgs.emacsPackagesNg) inherit-local;
   inherit (pkgs.nixBufferBuilders) withPackages;
   drv = import (builtins.toPath defnix) {};
-  packageEnv = pkg: lib.overrideDerivation pkg (old: {
+  packageEnv = pkg: overrideDerivation pkg (old: {
     phases = [ "installPhase" ];
     # TODO: run configurePhase and shellHook here
-    installPhase = ''${pkgs.jq}/bin/jq -n env > $out'';
+    installPhase = "${pkgs.jq}/bin/jq -n env > $out";
   });
   withPackage = pkg: runCommand "dir-locals.el" {
     env = packageEnv pkg;
